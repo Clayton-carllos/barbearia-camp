736506e66f8bc94b667ff22165929b49581c36f5
Clayton-carllos <clayton.carllos@gmail.com>
2025-01-09 17:37:31 -0300

    Correcao data e horario

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import csv
 from io import StringIO
 from flask import Response
 from flask_migrate import Migrate
-
+from sqlalchemy import and_
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'  # Necessário para usar flash()
@@ -50,163 +50,6 @@ with app.app_context():
         print('Usuário admin criado com sucesso!')
     else:
         print('Usuário admin já existe.')
-
-# Rota para exibir o formulário de agendamento
-@app.route('/')
-def index():
-    return render_template('formulario.html')
-
-# Rota para processar o agendamento
-@app.route('/agendar', methods=['POST'])
-def agendar():
-    nome = request.form['nome']
-    telefone = request.form['telefone']
-    email = request.form['email']
-    data = datetime.strptime(request.form['data'], '%Y-%m-%d').date()
-    horario = request.form['horario']
-    mensagem = request.form.get('mensagem')
-
-    # Criação de um novo agendamento
-    novo_agendamento = Agendamento(
-        nome=nome, telefone=telefone, email=email, data=data, horario=horario, mensagem=mensagem
-    )
-
-    # Salvar no banco de dados
-    db.session.add(novo_agendamento)
-    db.session.commit()
-
-    # Mensagem flash para informar o sucesso
-    flash('Agendamento realizado com sucesso!.', 'success')
-
-    return redirect(url_for('index'))  # Redireciona de volta ao formulário
-
-# Rota para exibir o formulário de adicionar usuário
-@app.route('/adicionar_usuario', methods=['GET', 'POST'])
-def adicionar_usuario():
-    
-    if 'usuario_id' not in session:
-        flash('Você precisa estar logado para adicionar um usuário.', 'warning')
-        return redirect(url_for('login'))
-
-    if request.method == 'POST':
-        username = request.form['username']
-        senha = request.form['senha']
-
-        # Verificar se o username já existe
-        if Usuario.query.filter_by(username=username).first():
-            flash('O nome de usuário já está em uso. Escolha outro.', 'danger')
-            return render_template('adicionar_usuario.html')
-
-        # Criação do hash da senha
-        senha_hash = generate_password_hash(senha)
-
-        # Criar o novo usuário
-        novo_usuario = Usuario(username=username, senha=senha_hash)
-
-        try:
-            # Adicionar ao banco de dados
-            db.session.add(novo_usuario)
-            db.session.commit()
-
-            flash('Usuário criado com sucesso!', 'success')
-            return redirect(url_for('lista_usuarios'))  # Redireciona para a lista de usuários
-        except IntegrityError:
-            db.session.rollback()  # Desfaz a transação se ocorrer um erro
-            flash('Erro ao criar o usuário. Tente novamente.', 'danger')
-
-    return render_template('adicionar_usuario.html')  # Exibe o formulário para adicionar usuário
-
-# Rota para editar um usuário
-@app.route('/editar_usuario/<int:id>', methods=['GET', 'POST'])
-def editar_usuario(id):
-    usuario = Usuario.query.get_or_404(id)  # Busca o usuário pelo ID
-    if request.method == 'POST':
-        # Atualiza os campos do usuário
-        usuario.username = request.form['username']
-        usuario.senha = generate_password_hash(request.form['senha'])  # Atualiza a senha com hash
-
-        # Salva no banco de dados
-        db.session.commit()
-
-        flash('Usuário atualizado com sucesso!', 'success')
-        return redirect(url_for('lista_usuarios'))  # Redireciona para a lista de usuários
-
-    return render_template('editar_usuario.html', usuario=usuario)  # Exibe o formulário de edição
-
-# Rota para listar usuários
-@app.route('/usuarios')
-def lista_usuarios():
-    if 'usuario_id' not in session:
-        flash('Você precisa estar logado para acessar esta página.', 'warning')
-        return redirect(url_for('login'))
-    
-    # Recupera o nome do usuário logado
-    nome_usuario = session.get('username')  # Obtém o nome do usuário da sessão
-
-    usuarios = Usuario.query.all()  # Recupera todos os usuários do banco de dados
-    return render_template('lista_usuarios.html', usuarios=usuarios, nome_usuario=nome_usuario)
-
-# Rota para exibir o perfil de um usuário
-@app.route('/usuario/<int:id>')
-def perfil_usuario(id):
-    if 'usuario_id' not in session:
-        flash('Você precisa estar logado para acessar esta página.', 'warning')
-        return redirect(url_for('login'))
-
-    usuario = Usuario.query.get_or_404(id)  # Recupera o usuário pelo ID
-    return render_template('perfil_usuario.html', usuario=usuario)
-
-# Rota para deletar um usuário
-@app.route('/deletar_usuario/<int:id>', methods=['GET'])
-def deletar_usuario(id):
-    if 'usuario_id' not in session:
-        flash('Você precisa estar logado para realizar esta ação.', 'warning')
-        return redirect(url_for('login'))
-
-    usuario = Usuario.query.get_or_404(id)  # Recupera o usuário pelo ID
-    db.session.delete(usuario)  # Exclui o usuário
-    db.session.commit()  # Salva a mudança no banco de dados
-    flash('Usuário excluído com sucesso!', 'success')
-    return redirect(url_for('lista_usuarios'))  # Redireciona para a lista de usuários
-
-# Rota para exibir a página de login
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        senha = request.form['senha']
-        usuario = Usuario.query.filter_by(username=username).first()
-        
-        if usuario and check_password_hash(usuario.senha, senha):
-            session['usuario_id'] = usuario.id
-            session['username'] = usuario.username
-            flash('Login realizado com sucesso!', 'success')
-            return redirect(url_for('home'))
-        else:
-            flash('Usuário ou senha inválidos.', 'danger')
-    
-    return render_template('login.html')
-
-# Rota para exibir a lista de agendamentos
-@app.route('/agendamentos')
-def lista_agendamentos():
-    if 'usuario_id' not in session:
-        flash('Você precisa estar logado para acessar esta página.', 'warning')
-        return redirect(url_for('login'))
-    
-    # Recupera o nome do usuário logado
-    nome_usuario = session.get('username')  # Obtém o nome do usuário da sessão
-
-    filtro = request.args.get('filtro', default=None)
-    
-    if filtro == 'hoje':
-        # Filtra apenas os agendamentos de hoje
-        agendamentos = Agendamento.query.filter(Agendamento.data == datetime.today().date()).all()
-    else:
-        # Caso contrário, exibe todos os agendamentos
-        agendamentos = Agendamento.query.all()
-
-    return render_template('lista_agendamentos.html', agendamentos=agendamentos, nome_usuario=nome_usuario)
 
 # Rota para exibir a Home
 @app.route('/home')
@@ -250,15 +93,15 @@ def home():
     
     # Pega os 3 próximos agendamentos
     proximos_agendamentos = (
-    Agendamento.query
-    .filter(
-        (Agendamento.data > datetime.today().date()) | 
-        ((Agendamento.data == datetime.today().date()) & (Agendamento.horario > datetime.now().time()))
+        Agendamento.query
+        .filter(
+            (Agendamento.data > datetime.today().date()) | 
+            ((Agendamento.data == datetime.today().date()) & (Agendamento.horario > agora.strftime('%H:%M')))
+        )
+        .order_by(Agendamento.data, Agendamento.horario)
+        .limit(3)
+        .all()
     )
-    .order_by(Agendamento.data, Agendamento.horario)
-    .limit(3)
-    .all()
-)
 
     return render_template(
         'home.html', 
